774b5a0d20ddbd10dc8066dcd8daec4c3cf03d0f
Milk-Cool <milan-pl@mail.ru>
2024-10-08 09:20:50 +0300

    register models

--- web/app/admin.py
+++ web/app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Alumni, Employer, Event, FullName, Topic, Vacancy
 
 # Register your models here.
+
+admin.site.register(Alumni)
+admin.site.register(Employer)
+admin.site.register(Event)
+admin.site.register(FullName)
+admin.site.register(Topic)
+admin.site.register(Vacancy)
---
+++ web/app/models/__init__.py
@@ -0,0 +1,6 @@
+from .alumni import Alumni
+from .employer import Employer
+from .event import Event
+from .full_name import FullName
+from .topic import Topic
+from .vacancy import Vacancy
