4cd4329863062a9ed1f681377a2db32ca5968200
Milk-Cool <milan-pl@mail.ru>
2024-10-08 09:47:01 +0300

    add __str__ function to fullname

--- web/app/models/full_name.py
+++ web/app/models/full_name.py
@@ -4,3 +4,6 @@ from django.db import models
 class FullName(models.Model):
     first_name = models.CharField(max_length=32)
     second_name = models.CharField(max_length=32)
+
+    def __str__(self):
+        return self.first_name + " " + self.second_name
