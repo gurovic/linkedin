e295c5a109ff98c713e717055b04a8eb0f3be7c4
Sundew999666 <sunlive999666@gmail.com>
2024-10-08 08:10:51 +0300

    added models

---
+++ web/app/models/request.py
@@ -0,0 +1,10 @@
+import datetime
+from django.db import models
+from user import User
+from rights import Right
+
+class Request(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField(default=datetime.date.today)
+    right_asked = models.ForeignKey(Right, on_delete=models.CASCADE)
+    desc = models.TextField()
