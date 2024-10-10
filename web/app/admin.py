f2468197d502e2cf61396ebc0080ba68f7d7449b
AlexanderGPo <alexanderg.povarov@gmail.com>
2024-10-10 23:25:11 +0300

    updating setting and registing models

--- web/app/admin.py
+++ web/app/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import Country, MajorSubject, School, StudentSchool
 
-# Register your models here.
+admin.site.register([Country, MajorSubject, School, StudentSchool])
