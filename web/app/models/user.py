7c95e0ca7608324e1b642f80d25fe042727d9c1b
Sundew999666 <sunlive999666@gmail.com>
2024-10-11 22:46:34 +0300

    urls changed

--- web/app/models/user.py
+++ web/app/models/user.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import get_user_model
 from django.db import models
+from . import rights
 
 class User(get_user_model()):
     def __str__(self):
         return self.name
+    rights = models.ManyToManyField(rights.Right)
--- web/web/urls.py
+++ web/web/urls.py
@@ -1,10 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from ..app.views import view_request
-from ..app.forms import new_request_form
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('request/', view_request, name='request'),
-    path('request_form/', new_request_form, name='request_form'),
 ]
