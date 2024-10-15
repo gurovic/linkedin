feb3e258c0396451bfea94083c72fb7c19809163
il-an <150121643+il-an@users.noreply.github.com>
2024-10-15 08:47:43 +0300

    resolved merge conflicts and fixed the project

--- web/app/urls.py
+++ web/app/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 
 from app.views.event_creation import event_creation
 from app.views.event_list import event_list
-from app.views.friends_in_university import friends_in_university
+from app.views.friends_in_university import get_friends
 
 urlpatterns = [
     path("events/", event_list, name="index"),
     path("events/new/", event_creation, name="event_creation"),
-    path("friends", friends_in_university.get_friends, name="friends in university"),
+    path("friends", get_friends, name="friends in university"),
 ]
--- web/web/settings.py
+++ web/web/settings.py
@@ -41,12 +41,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
 ]
 
 MIDDLEWARE = [
