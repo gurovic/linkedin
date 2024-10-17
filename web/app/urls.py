76cd94201f45fb5ecfb1298c1424d4300827625b
Sundew999666 <sunlive999666@gmail.com>
2024-10-17 19:54:34 +0300

    Final form changes before merge

--- web/app/urls.py
+++ web/app/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from .views.view_request import request_view
-from .forms.new_request_form import RequestForm
+from .views.create_request_form import create_request
 
 urlpatterns = [
     path('request/', request_view, name='request'),
-    path('request_form/', RequestForm, name='request_form'),
+    path('request_form/', create_request, name='request_form'),
 ]
