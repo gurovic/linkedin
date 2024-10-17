b8719da38088128b5f2639ac78c14ad7052401e1
Victor Kiryushkin <victorkiryushkin@Victors-MacBook-Air.local>
2024-10-17 23:21:03 +0300

    updating model company

--- web/app/models/company.py
+++ web/app/models/company.py
@@ -1,7 +1,24 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django import forms
 
-class Country(models.Model):
+class Vacancy(models.Model):
+    MAJOR_CHOICES = [
+        ('CS', 'Computer Science'),
+        ('MATH', 'Mathematics'),
+        ('ENG', 'English'),
+        ('RU', 'Russian Language'),
+        ('ART', 'Arts'),
+        ('BIO', 'Biology'),
+        ('PHY', 'Physics'),
+        ('CHEM', 'Chemistry'),
+    ]
+    name = models.CharField(max_length=100)
+    description = models.TextField(null=True,blank=True)
+    needed_majorsubject=forms.ChoiceField(MAJOR_CHOICES)
+
+
+class Company(models.Model):
     COUNTRY_CHOICES = [
         ('RU', 'Russian Federation'),
         ('US', 'United States'),
@@ -14,35 +31,13 @@ class Country(models.Model):
         ('IN', 'India'),
         ('BR', 'Brazil'),
     ]
-    name = models.CharField(max_length=2, choices=COUNTRY_CHOICES, default='RU')
 
-    def __str__(self):
-        return self.get_name_display()
-
-class MajorSubject(models.Model):
-    MAJOR_CHOICES = [
-        ('CS', 'Computer Science'),
-        ('MATH', 'Mathematics'),
-        ('ENG', 'English'),
-        ('RU', 'Russian Language'),
-        ('ART', 'Arts'),
-        ('BIO', 'Biology'),
-        ('PHY', 'Physics'),
-        ('CHEM', 'Chemistry'),
-    ]
-
-    subject = models.CharField(max_length=4, choices=MAJOR_CHOICES)
-
-class Vacancy(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(null=True,blank=True)
-    needed_majorsubject=models.ManyToManyField(MajorSubject)
-class Company(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField(null=True,blank=True)
-    country = models.ForeignKey(Country, on_delete=models.CASCADE)
-    vacancies = models.ManyToManyField(Vacancy)
+    country = forms.ChoiceField(COUNTRY_CHOICES)
+    vacancies = models.ForeignKey(Vacancy)
     current_workers = models.ForeignKey(User, on_delete=models.CASCADE, related_name='companies')
+
     def __str__(self):
         return self.name
 
