1246eacf720aa0a3df78710ba85be74cc0da4c0c
ArtScienceMK <2026kulakov.am@student.letovo.ru>
2024-10-24 15:45:03 +0300

    Changes

--- web/web/urls.py
+++ web/web/urls.py
@@ -1,5 +1,7 @@
 from django.urls import include, path
+from django.contrib import admin
 
 urlpatterns = [
-    path("app/", include("app.urls"))
+    path("app/", include("app.urls")),
+    path('admin/', admin.site.urls)
 ]
