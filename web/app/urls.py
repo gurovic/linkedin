f0e44b852faa982fa68fb2499685fe72254e50d3
ArtScienceMK <2026kulakov.am@student.letovo.ru>
2024-10-24 09:01:53 +0300

    Changed by comments

--- web/app/urls.py
+++ web/app/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
-from .views import friends_in_university, sign_up
-from .views.view_request import request_view
+from .views import sign_up
 from .views.create_request_form import create_request
+from .views.view_request import request_view
+
 urlpatterns = [
-    path("friends", friends_in_university.get_friends, name="friends in university"),
-    path('sign_up', sign_up.sign_up, name='sign_up'), 
-  path('request/', request_view, name='request'),
+    path('sign_up', sign_up.sign_up, name='sign_up'),
+    path('request/', request_view, name='request'),
     path('request_form/', create_request, name='request_form'),
 ]
--- web/web/urls.py
+++ web/web/urls.py
@@ -1,9 +1,5 @@
-from django.contrib import admin
 from django.urls import include, path
-from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-  path('university/', include("app.urls")),
-    path('accounts/', include("app.urls")),
-  path("app/", include("app.urls"))]
+    path("app/", include("app.urls"))
+]
