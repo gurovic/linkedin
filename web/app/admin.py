16467049b81d762c26b2a1bdbb84f67259a61c6e
Victor Kiryushkin <victorkiryushkin@Victors-MacBook-Air.local>
2024-10-29 00:54:56 +0300

    admin update

--- web/app/admin.py
+++ web/app/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from .models import Vacancy, MajorSubject, Company, Country
+from .models import Vacancy,Company
 
-admin.site.register([Vacancy, MajorSubject, Company, Country])
+admin.site.register([Vacancy,Company])
