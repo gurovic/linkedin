72bb846498dcf32051fa4635389eeee35c05b58a
Danilov Nikita <nik.danilov.nn@gmail.com>
2024-10-29 00:23:54 +0300

    adding_tags2

--- web/app/models/tag.py
+++ web/app/models/tag.py
@@ -1,17 +1,9 @@
 from django.db import models
 
 class Tag(models.Model):
-    """TAGS_CHOICES = [
-        ("PYTHON DEVELOPER", "python developer"),
-        ("WEB DESIGNER", "web designer"),
-        ("C1 ENGLISH", "C1 English")
-    ]"""
 
     skill_owners = models.ManyToManyField("auth.User", related_name = "tags")
-    name = models.CharField(max_length = 50)#, choices = TAGS_CHOICES)
-
-    def is_owner(self, user_id):
-        return list(self.skill_owners).count(user_id) > 0
+    name = models.CharField(max_length = 50, unique = True)
 
     def __str__(self):
         return self.name
--- web/app/urls.py
+++ web/app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
 
-from .views.view_tags import tags_view
+from .views.view_tags import tags_view, add_tag_to_user
 
 urlpatterns = [
     path('<int:user_id>/', tags_view, name = 'user_profile'),
+    path('<int:user_id>/add_tag/', add_tag_to_user, name = 'adding_tags'),
 ]
