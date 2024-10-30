8b27176f2950917595ea64f5f7e12f38534e4d92
Sue <sue-istratova@mail.ru>
2024-10-30 23:41:27 +0300

    the form with career path of graduators

---
+++ web/app/forms/forms.py
@@ -0,0 +1,12 @@
+# forms.py
+from django import forms
+from .models import JobExperience
+
+class JobExperienceForm(forms.ModelForm):
+    class Meta:
+        model = JobExperience
+        fields = ['company_name', 'start_year', 'end_year', 'position']
+        widgets = {
+            'start_year': forms.NumberInput(attrs={'min': 1900, 'max': 2100}),
+            'end_year': forms.NumberInput(attrs={'min': 1900, 'max': 2100}),
+        }
--- web/app/urls.py
+++ web/app/urls.py
@@ -5,4 +5,5 @@ from .views.create_request_form import create_request
 urlpatterns = [
     path('request/', request_view, name='request'),
     path('request_form/', create_request, name='request_form'),
+    path('job_experience/', view.job_experience_view, name='job_experience'),
 ]
