af1a1d9691f879774746f72fc3f3573243d30b86
Владимир Гуровиц <gurovic@gmail.com>
2024-11-02 09:12:02 +0300

    deleted trash app

--- web/main_app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import home, registration
-
-urlpatterns = [
-    path('', home, name='home'),
-    path('registration/', registration, name='registration')
-]
