56caf879c545f0e261512b4af7ef22bc255d8db1
Danilov Nikita <nik.danilov.nn@gmail.com>
2024-11-03 23:35:10 +0300

    -S

--- web/app/admin.py
+++ web/app/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from .models import Request
 from .models import Answer
+from .models import Event
+from .models import Tag
 
 # Register your models here.
-from .models import Tag
+admin.site.register(Event)
 admin.site.register(Tag)
 admin.site.register(Request)
 admin.site.register(Answer)
--- web/app/models/__init__.py
+++ web/app/models/__init__.py
@@ -2,3 +2,4 @@ from .tag import Tag
 from .answer import Answer
 from .requests import Request
 from .typeofuser import TypeOfUser
+from .event import Event
