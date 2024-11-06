29e4a1212930bb5355476fddd2286464ac30809e
Владимир Гуровиц <gurovic@gmail.com>
2024-11-06 15:50:33 +0300

    api test

--- web/app/urls.py
+++ web/app/urls.py
@@ -1,10 +1,27 @@
-from django.urls import path
+from django.urls import path, include
 from .views.company_list import company_list
 from .views.view_request import request_view
 from .views.create_request_form import create_request
 from .views.view_tags import tags_view, add_tag_to_user
 from .views.event_list import event_list
 from .views.job_experience import job_experience_view
+from django.contrib.auth.models import User
+from rest_framework import routers, serializers, viewsets
+
+# Serializers define the API representation.
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'email', 'is_staff']
+
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
 
 urlpatterns = [
     path("companies/", company_list, name='company_list'),
@@ -14,4 +31,7 @@ urlpatterns = [
     path('request_form/', create_request, name='request_form'),
     path('event_list/', event_list, name='index'),
     path('job_experience/', job_experience_view, name='job_experience'),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+
 ]
