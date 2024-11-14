0da2e215c6b55cddac4b16944e767c8793650226
brabrazavr <platonadzhiev@gmail.com>
2024-11-15 01:59:46 +0300

    minor changes in representation of main page before and after registration

--- web/app/views/home.py
+++ web/app/views/home.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 #from django import forms
+from django.contrib.auth.models import User
 
 def home(request):
+    users = User.objects.all()
     return render(request, 'app/home.html')
 
+
 def registration(request):
-    return render(request, 'app/register.html')
+    return render(request, 'app/register.html', {'users' : users})
