d48d8f647a8aa9215173f255680938fb4d234d5b
Victor Kiryushkin <victorkiryushkin@Victors-MacBook-Air.local>
2024-11-15 00:06:12 +0300

    updating admin

--- web/app/admin.py
+++ web/app/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Vacancy, Company, Request, Answer
+from .models import Vacancy, Company, Request, Answer, University, UniversityStudent
 
 # Register your models here.
 admin.site.register(Vacancy)
 admin.site.register(Company)
 admin.site.register(Request)
 admin.site.register(Answer)
+admin.site.register(University)
+admin.site.register(UniversityStudent)
