e639eeac9586cbfc208d1cd63511979898e8effa
hizxle <torbionic@gmail.com>
2024-11-23 15:05:22 +0300

    removing files

--- web/app/views/views.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import generics
-from app.models.alumni_model import Alumni
-from ..serializers.AlumniSerializer import AlumniSerializer
-
-class AlumniListView(generics.ListAPIView):
-    queryset = Alumni.objects.all()
-    serializer_class = AlumniSerializer
