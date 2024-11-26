dd57bc60c18b3c9a9b401c1b1924a993552717b0
Victor Kiryushkin <victorkiryushkin@Victors-MacBook-Air.local>
2024-11-26 08:57:06 +0300

    new conflict resolve

--- web/web/urls.py
+++ web/web/urls.py
@@ -5,11 +5,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-<<<<<<< HEAD
     path("", include("app.urls")),
-]
-=======
-    path("app/", include("app.urls")),
-    path('', include('app.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
->>>>>>> 2b0e6379b2bcee0c3990d4099650b99d11e70b68
