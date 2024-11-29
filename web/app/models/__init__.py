62f824d48ca3b583928b0d8448f7cd5ac5b32a90
kiryushkinvictor <kiryushkinvictor@gmail.com>
2024-11-29 11:04:39 +0300

    adding university api

---
+++ web/app/models/__init__.py
@@ -0,0 +1,11 @@
+from .school import School, MajorSubject
+from .student_schools import StudentSchool
+from .company import Company, Vacancy
+from .tag import Tag
+from .answer import Answer
+from .requests import Request
+from .typeofuser import TypeOfUser
+from .university import University
+from .universitystudent import UniversityStudent
+from .event import Event
+from .job_experience import JobExperience
