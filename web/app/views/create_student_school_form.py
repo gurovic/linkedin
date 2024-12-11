0b743986f8b26ca5b6e01d7caec22f68bc409632
il-an <150121643+il-an@users.noreply.github.com>
2024-12-11 23:39:20 +0300

    added user editable account

--- web/app/views/create_student_school_form.py
+++ web/app/views/create_student_school_form.py
@@ -1,18 +1,25 @@
-from django.shortcuts import redirect, render
-from ..forms import StudentSchoolForm
-from .index import index
+from django.shortcuts import get_object_or_404, redirect, render
+
+from app.forms import StudentSchoolForm
+from app.models import StudentSchool
 
 
-def create_student_school(request):
+def create_or_edit_student_school(request, pk=None):
+    if pk:
+        student_school = get_object_or_404(StudentSchool, pk=pk)
+        if student_school.student != request.user:
+            return redirect("student_schools")
+    else:
+        student_school = None
+
     if request.method == "POST":
-        form = StudentSchoolForm(request.POST)
+        form = StudentSchoolForm(request.POST, instance=student_school)
         if form.is_valid():
             new_form = form.save(commit=False)
-            user = request.user
-            new_form.student = user
+            new_form.student = request.user
             new_form.save()
-            return redirect(index)
+            return redirect("student_schools")
     else:
-        form = StudentSchoolForm()
+        form = StudentSchoolForm(instance=student_school)
 
     return render(request, "app/student_school_form.html", {"form": form})
