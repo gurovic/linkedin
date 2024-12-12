f1bcfc01a053532e5a9084917b92f2de0e883581
hizxle <torbionic@gmail.com>
2024-12-12 22:53:12 +0300

    adding events

--- web/app/views/home.py
+++ web/app/views/home.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
+from ..models import Event
 
 def home(request):
     users = User.objects.all()
-    return render(request, 'app/home.html', {'users' : users})
+
+    event_objects = Event.objects.all()
+    events = [event for event in event_objects if not event.already_passed()]
+
+    for event in events:
+        if event.picture:
+            event.format_image_to_height(300)
+            event.save()
+
+    context = {
+        'users': users,
+        'event_list': events,
+    }
+
+    return render(request, 'app/home.html', context)
