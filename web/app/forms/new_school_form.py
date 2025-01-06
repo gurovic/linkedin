815a654713132c41748b40ddb60c0c900bb97a3c
brabrazavr <platonadzhiev@gmail.com>
2025-01-06 11:26:44 +0300

    changed the model School

--- web/app/forms/new_school_form.py
+++ web/app/forms/new_school_form.py
@@ -4,10 +4,10 @@ from ..models import School
 class SchoolForm(forms.ModelForm):
     class Meta:
         model = School
-        fields = ['name', 'desc', 'country', 'majors']
+        fields = ['name', 'desc', 'country', 'majors_available']
         widgets = {
             'name': forms.TextInput(attrs={'class': 'name_of_school'}),
             'desc': forms.Textarea(attrs={'class': 'description_of_school'}),
             'country': forms.Select(attrs={'class': 'country_of_school'}),
-            'majors': forms.CheckboxSelectMultiple(attrs={'class': 'major_of_school'}),
+            #'majors_available': forms.CheckboxSelectMultiple(attrs={'class': 'major_of_school'}),
         }
