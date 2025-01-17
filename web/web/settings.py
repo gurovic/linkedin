dced3ef2c1ceaca3d0477ba626716e6311de374a
Milk-Cool <milan-pl@mail.ru>
2025-01-17 17:12:56 +0300

    added .env support

--- web/web/settings.py
+++ web/web/settings.py
@@ -13,9 +13,12 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 
+import dotenv
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+dotenv.load_dotenv(override=False)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
