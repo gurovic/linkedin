0ff0cef257f06c7714ff0f8f0ea588504a034314
il-an <150121643+il-an@users.noreply.github.com>
2025-01-18 12:03:27 +0300

    script now handles termination

--- start.sh
+++ start.sh
@@ -1,6 +1,21 @@
-#!/bin/zsh
+#!/bin/bash
+
+cleanup() {
+    echo "Killing background processes..."
+    kill $django_pid
+    kill $ng_pid
+    exit 0
+}
+
+trap cleanup SIGINT SIGTERM
 
 cd ./web
 python3 manage.py runserver &
+django_pid=$!
+
 cd ../web_frontend
-ng serve
+npx ng serve &
+ng_pid=$!
+
+wait $django_pid
+wait $ng_pid
