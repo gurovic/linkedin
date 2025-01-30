c5e4531aba14b53cdeb67d3fb6b63aae09bda73a
Sundew999666 <sunlive999666@gmail.com>
2025-01-30 22:55:08 +0300

    fixed some bugs

--- web/app/views/api_ans_verification.py
+++ web/app/views/api_ans_verification.py
@@ -2,15 +2,14 @@ from rest_framework import status, viewsets
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from django.shortcuts import get_object_or_404
-from .models.alumni_verification_request import AlumniVerificationRequest
-from .serializers.verification_ans_serializer import AnsVerificationSerializer
+from ..models.alumni_verification_request import AlumniVerificationRequest
+from ..serializers.verification_ans_serializer import AnsVerificationSerializer
 from rest_framework.views import APIView
 
 
 class AnsVerificationView(APIView):
-    @action(detail=True, methods=['patch'])
-    def update_status(self, request, pk=None):
-        req = get_object_or_404(AlumniVerificationRequest, pk=pk)
+    def patch(self, request, request_id):
+        req = get_object_or_404(AlumniVerificationRequest, id=request_id)
         app_or_dec = request.data.get('approved')
 
         if app_or_dec is None:
