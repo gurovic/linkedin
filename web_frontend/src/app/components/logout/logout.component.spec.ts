644b4c2f3da25ca6389980694acb4b67891d76d1
Milk-Cool <coder.tomsk@gmail.com>
2025-01-30 22:52:55 +0300

    logout tests

--- web_frontend/src/app/components/logout/logout.component.spec.ts
+++ web_frontend/src/app/components/logout/logout.component.spec.ts
@@ -1,23 +1,38 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LogoutComponent } from './logout.component';
+import { Router } from '@angular/router';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('LogoutComponent', () => {
   let component: LogoutComponent;
   let fixture: ComponentFixture<LogoutComponent>;
+  let router: Router;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [LogoutComponent]
+      imports: [LogoutComponent, HttpClientTestingModule]
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(LogoutComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    router = TestBed.inject(Router);
+
+    // Setting the token here
+    localStorage.setItem("authToken", "toBeDeleted");
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should delete the session token', () => {
+    const navigateSpy = spyOn(router, 'navigate');
+
+    component.ngOnInit();
+    expect(navigateSpy).toHaveBeenCalledWith(['/']);
+    expect(localStorage.getItem("authToken")).toBeNull();
   });
 });
