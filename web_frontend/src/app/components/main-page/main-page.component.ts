0de9f0bb1d45e96a4eb814b0a09c66b082cad718
brabrazavr <platonadzhiev@gmail.com>
2025-02-02 13:42:24 +0300

    writing main page for unauthorized user

--- web_frontend/src/app/components/main-page/main-page.component.ts
+++ web_frontend/src/app/components/main-page/main-page.component.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
 import { FooterComponent } from '../footer/footer.component';
 import { ValuesComponent } from '../values/values.component';
 import { AboutSiteComponent } from '../about-site/about-site.component';
+import { LoginComponent } from '../login/login.component';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [FooterComponent, AboutSiteComponent, ValuesComponent],
+  imports: [FooterComponent, AboutSiteComponent, ValuesComponent, LoginComponent, NgIf],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.css'
 })
 export class MainPageComponent {
-
+  showLogin = false;
 }
