6ba03e3d70e1e21ff0a9d0d912684af6ce9aae87
brabrazavr <platonadzhiev@gmail.com>
2025-02-06 23:57:43 +0300

    rewriting models University, UniversityStudent

--- web/app/models/university.py
+++ web/app/models/university.py
@@ -1,7 +1,6 @@
 from django.db import models
 from multiselectfield import MultiSelectField
-from django import forms
-from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 
 class University(models.Model):
@@ -31,6 +30,26 @@ class University(models.Model):
     description = models.TextField(null=True, blank=True)
     country = models.CharField(max_length=100, default='Smth', choices=COUNTRY_CHOICES)
     majors_availible = MultiSelectField(max_length=100, default='Smth', choices=MAJOR_CHOICES)
+    lat = models.FloatField(null=True, blank=True)
+    lon = models.FloatField(null=True, blank=True)
+    country = models.CharField(max_length=100, blank=True)
+
+    '''The script country_from_coords is required for the code to work properly
+
+    def get_country_from_coords(self):
+        if self.lat is not None and self.lon is not None:
+            try:
+                from country_from_coords import get_country
+                return get_country(self.lat, self.lon) 
+            except ImportError:
+                raise ValidationError("The script country_from_coords.py is missing!")
+
+    def save(self, *args, **kwargs):
+        if not self.country and self.lat is not None and self.lon is not None:
+            self.country = self.get_country_from_coords()
+        super().save(*args, **kwargs)
+    
+    '''
 
     def __str__(self):
         return self.name
