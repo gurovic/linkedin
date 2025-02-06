9d6b19ad43bb16a99f9e25eb015a7dd417a28ad6
il-an <150121643+il-an@users.noreply.github.com>
2025-02-06 23:20:03 +0300

    added supplemental models for vacancy and tests

---
+++ web/app/models/language_test.py
@@ -0,0 +1,20 @@
+from django.db.utils import IntegrityError
+from django.test import TestCase
+
+from app.models.language import Language
+
+
+class LanguageTest(TestCase):
+    def setUp(self):
+        self.language = Language(name="example")
+
+    def test_language_creation(self):
+        self.assertEqual(self.language.name, "example")
+
+    def test_language_str(self):
+        self.assertEqual(str(self.language), "example")
+
+    def test_language_uniqueness(self):
+        with self.assertRaises(IntegrityError):
+            language = Language(name="example")
+            language.save()
