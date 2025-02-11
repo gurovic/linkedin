00d87a2a1c682118c641e58da73b8d36c42cda53
il-an <150121643+il-an@users.noreply.github.com>
2025-02-11 09:00:21 +0300

    removed the form

--- web/app/views/search_api.py
+++ web/app/views/search_api.py
@@ -3,46 +3,43 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from app.forms.search_form import UserSearchForm
 from app.serializers.user_serializer import UserDetailSerializer
 
 
 class UserSearchAPIView(APIView):
     def get(self, request, *args, **kwargs):
-        form = UserSearchForm(request.GET or None)
+        query = request.GET.get("query", None)
+        university = request.GET.get("university", None)
+        school = request.GET.get("school", None)
+
         users = User.objects.all()
 
-        if form.is_valid():
-            query = form.cleaned_data.get("query")
-            university = form.cleaned_data.get("university")
-            school = form.cleaned_data.get("school")
-
-            if query:
-                users = (
-                    users.filter(
-                        last_name__icontains=query,
-                    )
-                    | users.filter(
-                        first_name__icontains=query,
-                    )
-                    | users.filter(
-                        first_name__icontains=query.split(" ")[0],
-                    )
-                    | users.filter(
-                        last_name__icontains=query.split(" ")[0],
-                    )
-                    | users.filter(
-                        first_name__icontains=query.split(" ")[-1],
-                    )
-                    | users.filter(
-                        last_name__icontains=query.split(" ")[-1],
-                    )
+        if query:
+            users = (
+                users.filter(
+                    last_name__icontains=query,
                 )
+                | users.filter(
+                    first_name__icontains=query,
+                )
+                | users.filter(
+                    first_name__icontains=query.split(" ")[0],
+                )
+                | users.filter(
+                    last_name__icontains=query.split(" ")[0],
+                )
+                | users.filter(
+                    first_name__icontains=query.split(" ")[-1],
+                )
+                | users.filter(
+                    last_name__icontains=query.split(" ")[-1],
+                )
+            )
 
-            if university:
-                users = users.filter(student__university=university)
-            if school:
-                users = users.filter(studentschool__school=school)
+        if university:
+            users = users.filter(student__university=university)
+        if school:
+            users = users.filter(studentschool__school=school)
 
         serializer = UserDetailSerializer(users, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
