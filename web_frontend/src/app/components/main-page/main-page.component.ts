c39c0021f56597d7512a94fc3ebaecf11e1fe917
il-an <150121643+il-an@users.noreply.github.com>
2025-02-14 08:42:19 +0300

    removed duplicate footer

--- web_frontend/src/app/components/main-page/main-page.component.ts
+++ web_frontend/src/app/components/main-page/main-page.component.ts
@@ -4,12 +4,11 @@ import { ValuesComponent } from '../values/values.component';
 import { AboutSiteComponent } from '../about-site/about-site.component';
 import { LoginComponent } from '../login/login.component';
 import {MapComponent} from '../map/map.component';
-import {FooterComponent} from '../footer/footer.component';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [FooterComponent, AboutSiteComponent, ValuesComponent, LoginComponent, NgIf, MapComponent],
+  imports: [AboutSiteComponent, ValuesComponent, LoginComponent, NgIf, MapComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.css'
 })
