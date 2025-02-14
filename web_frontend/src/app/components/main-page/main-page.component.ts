8e3e2d2f63865df489c3208deb596f21b406f3cd
il-an <150121643+il-an@users.noreply.github.com>
2025-02-14 08:26:48 +0300

    added map to angular

--- web_frontend/src/app/components/main-page/main-page.component.ts
+++ web_frontend/src/app/components/main-page/main-page.component.ts
@@ -4,11 +4,12 @@ import { FooterComponent } from '../footer/footer.component';
 import { ValuesComponent } from '../values/values.component';
 import { AboutSiteComponent } from '../about-site/about-site.component';
 import { LoginComponent } from '../login/login.component';
+import {MapComponent} from '../map/map.component';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [FooterComponent, AboutSiteComponent, ValuesComponent, LoginComponent, NgIf],
+  imports: [FooterComponent, AboutSiteComponent, ValuesComponent, LoginComponent, NgIf, MapComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.css'
 })
