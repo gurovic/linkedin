11a5f253d3a719c1cee9a1fc52bfc1d6cc32e2b8
Serg.Goncharenko <sk.goncharenko@gmail.com>
2025-02-27 23:42:18 +0300

    Added test for AllVacanciesView

--- web/app/views/api_vacancy_test.py
+++ web/app/views/api_vacancy_test.py
@@ -12,19 +12,40 @@ from ..models.language import Language
 class VacancyApiTests(APITestCase):
     def setUp(self):
         self.user = User.objects.create_user(username='testuser', password='testpassword')
-        self.company = Company.objects.create(name="Company 1", description="Description 1",)
-        self.major = Major.objects.create(name="Major 1")
+        self.company1 = Company.objects.create(name="Company 1", description="Company description 1",)
+        self.company2 = Company.objects.create(name="Company 1", description="Company description 2",)
+        self.major1 = Major.objects.create(name="Major 1")
+        self.major2 = Major.objects.create(name="Major 2")
         self.language = Language.objects.create(name="English")
-        self.vacancy = Vacancy.objects.create(
+        self.vacancy1 = Vacancy.objects.create(
             name="Vacancy 1",
             description="Description 1",
-            required_majors=self.major.id,
-            company=self.company.id,
+            required_majors=self.major1.id,
+            company=self.company1.id,
             expiration_date=timezone.now(),
             required_language=self.language.id,
-            contacts='<EMAIL>'
+            contacts='<EMAIL1>'
+        )
+        self.vacancy2 = Vacancy.objects.create(
+            name="Vacancy 2",
+            description="Description 2",
+            required_majors=self.major2.id,
+            company=self.company2.id,
+            expiration_date=timezone.now(),
+            required_language=self.language.id,
+            contacts='<EMAIL2>'
         )
 
     def get_vacancy(self):
-        serializer = VacancySerializer(vacancy_id=self.vacancy.id)
-        self.assertEqual(serializer.data, self.vacancy.to_dict())
+        serializer = VacancySerializer(vacancy_id=self.vacancy1.id)
+        self.assertEqual(serializer.data, self.vacancy1.to_dict())
+
+    def get_all_vacancies(self):
+        url = reverse('all_vacancies')
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 2)
+
+        self.assertEqual(response.data[0]['name'], self.vacancy1.name)
+        self.assertEqual(response.data[1]['name'], self.vacancy2.name)
