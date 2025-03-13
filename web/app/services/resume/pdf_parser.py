45bdf6e67baee3cb4bb69f58bdc577e2b35af31c
brabrazavr <platonadzhiev@gmail.com>
2025-03-13 23:53:15 +0300

    minor changes

--- web/app/services/resume/pdf_parser.py
+++ web/app/services/resume/pdf_parser.py
@@ -1,6 +1,6 @@
 from io import BytesIO
 from pdfminer.high_level import extract_text
-from resume.resume_analyzer import analyze_resume
+from app.services.resume.resume_analyzer import analyze_resume
 
 def parse_pdf(file):
     pdf_bytes = BytesIO(file.read())
