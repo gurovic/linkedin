ad3bf92990b61c703ca2e6b4ddbda028f130a0a8
andBush <bushin-08@mail.ru>
2025-03-24 22:01:44 +0300

    forgot /

--- web/app/models/alumni_face.py
+++ web/app/models/alumni_face.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class AlumniFace(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image_path = models.ImageField(upload_to="alumni_faces")
+    image_path = models.ImageField(upload_to="alumni_faces/")
     x = models.IntegerField()
     y = models.IntegerField()
     w = models.IntegerField()
