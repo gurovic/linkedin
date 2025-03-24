50c15cebe75e73cf26ad80572cdba68433d0103b
andBush <bushin-08@mail.ru>
2025-03-24 13:46:52 +0300

    small bug fixed

--- web/app/models/alumni_face_user.py
+++ web/app/models/alumni_face_user.py
@@ -1,9 +1,8 @@
 from django.contrib.auth.models import User
+from .alumni_face import AlumniFace
 from django.db import models
 
 
 class AlumniFaceUser(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image_path = models.ImageField(
-        upload_to="alumnus_faces_photo/",
-    )
+    image = models.OneToOneField(AlumniFace, on_delete=models.CASCADE)
