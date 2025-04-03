42aa73e1930220771db7f9228eede18fb2f37fcf
kiryushkinvictor <kiryushkinvictor@gmail.com>
2025-04-03 22:42:35 +0300

    fixing scill

--- web/app/models/skill.py
+++ web/app/models/skill.py
@@ -1,27 +1,27 @@
-    from django.db import models
+from django.db import models
 
 
-    class Skill(models.Model):
-        name = models.CharField(max_length=150, unique=True)
+class Skill(models.Model):
+    name = models.CharField(max_length=150, unique=True)
 
-        def __str__(self):
-            return self.name
+    def __str__(self):
+        return self.name
 
 
-    class UserSkill(models.Model):
-        user = models.ForeignKey(
-            "auth.User",
-            on_delete=models.CASCADE,
-            related_name="userskills",
-        )
-        skill = models.ForeignKey(
-            Skill,
-            on_delete=models.CASCADE,
-            related_name="userskills",
-        )
+class UserSkill(models.Model):
+    user = models.ForeignKey(
+        "auth.User",
+        on_delete=models.CASCADE,
+        related_name="userskills",
+    )
+    skill = models.ForeignKey(
+        Skill,
+        on_delete=models.CASCADE,
+        related_name="userskills",
+    )
 
-        class Meta:
-            unique_together = ("user", "skill")
+    class Meta:
+        unique_together = ("user", "skill")
 
-        def __str__(self):
-            return f"{self.user.username} - {self.skill.name}"
+    def __str__(self):
+        return f"{self.user.username} - {self.skill.name}"
