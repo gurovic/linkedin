ec49d79f33b5002768bed717b78eafd7ff76d016
alexgontar <als.gontar@gmail.com>
2025-04-07 16:27:38 +0300

    deleting unnecessary fields

--- web/app/models/alumni_face.py
+++ web/app/models/alumni_face.py
@@ -5,8 +5,4 @@ from django.db import models
 class AlumniFace(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     image_path = models.ImageField(upload_to="alumni_faces/")
-    x = models.IntegerField()
-    y = models.IntegerField()
-    w = models.IntegerField()
-    h = models.IntegerField()
 
