2e8483dcb4448b2b799c97ba345aed9ca6ae6083
il-an <150121643+il-an@users.noreply.github.com>
2025-04-15 00:36:28 +0300

    made account work

--- web/app/views/angular_uneditable_account_api.py
+++ web/app/views/angular_uneditable_account_api.py
@@ -1,22 +1,27 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth.models import User
 from ..serializers.angular_uneditable_account_serializer import AngularUserDetailSerializer
 
 
 @api_view(['GET'])
-def user_detail_api_view(request, user_id = None):
-    if user_id is None:
-        user = request.user
-    else:
-        try:
-            user = User.objects.get(id=user_id)
-        except User.DoesNotExist:
-            return Response(
-                {"detail": "User not found"},
-                status=status.HTTP_404_NOT_FOUND,
-            )
+def user_detail_api_view(request, user_id):
+    try:
+        user = User.objects.get(id=user_id)
+    except User.DoesNotExist:
+        return Response(
+            {"detail": "User not found"},
+            status=status.HTTP_404_NOT_FOUND,
+        )
 
+    serializer = AngularUserDetailSerializer(user)
+    return Response(serializer.data)
+
+@api_view(["GET"])
+@permission_classes([IsAuthenticated])
+def self_detail_api_view(request):
+    user = request.user
     serializer = AngularUserDetailSerializer(user)
     return Response(serializer.data)
