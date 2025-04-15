0d2c27c9f2996923888e72a5db206bcb657fb25b
il-an <150121643+il-an@users.noreply.github.com>
2025-04-15 09:59:16 +0300

    fixed permissions on user api

--- web/app/views/user_api_view.py
+++ web/app/views/user_api_view.py
@@ -1,11 +1,13 @@
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404
+from rest_framework.decorators import permission_classes
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from ..serializers import UserDetailSerializer
 
-
+@permission_classes([IsAuthenticated])
 class UserDetailView(APIView):
     def get(self, request, user_id):
         user = get_object_or_404(User, id=user_id)
