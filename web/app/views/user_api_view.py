91ced18069a0e0bb3cef2be1dc442a27d9aaa341
il-an <150121643+il-an@users.noreply.github.com>
2025-04-15 11:16:36 +0300

    permissions fix

--- web/app/views/user_api_view.py
+++ web/app/views/user_api_view.py
@@ -1,14 +1,15 @@
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404
-from rest_framework.decorators import permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from ..serializers import UserDetailSerializer
 
-@permission_classes([IsAuthenticated])
+
 class UserDetailView(APIView):
+    permission_classes = [IsAuthenticated]
+
     def get(self, request, user_id):
         user = get_object_or_404(User, id=user_id)
         serializer = UserDetailSerializer(user)
