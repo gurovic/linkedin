63f0e6e1a2af8f557f54d9cbe3aa3930c899625d
alexgontar <als.gontar@gmail.com>
2025-04-25 09:45:01 +0300

    fixes of test

--- web/app/views/api_job_experience_test.py
+++ web/app/views/api_job_experience_test.py
@@ -2,6 +2,8 @@ from django.urls import reverse
 from rest_framework.test import APITestCase, APIClient
 from django.contrib.auth.models import User
 from ..models import JobExperience
+from rest_framework.authtoken.models import Token
+
 
 class JobExperienceAPITests(APITestCase):
     def setUp(self):
@@ -9,6 +11,10 @@ class JobExperienceAPITests(APITestCase):
             username='testuser',
             password='testpass123'
         )
+
+        self.token = Token.objects.create(user=self.user)
+        self.client = APIClient()
+        self.client.credentials(HTTP_AUTHORIZATION=f'Token {self.token.key}')
 
         self.experience1 = JobExperience.objects.create(
             user=self.user,
@@ -25,36 +31,39 @@ class JobExperienceAPITests(APITestCase):
             position='Senior Developer'
         )
 
-        self.client = APIClient()
         self.url = reverse('job_experience')
+
 
     def test_get_all_job_experiences(self):
         response = self.client.get(self.url)
 
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(response.data), 2)
+        self.assertEqual(len(response.data['results']), 2)  # Используем ['results'] если включена пагинация
 
-        self.assertEqual(response.data[0]['company_name'], 'Company A')
-        self.assertEqual(response.data[0]['position'], 'Developer')
-        self.assertEqual(response.data[0]['start_year'], 2018)
-        self.assertEqual(response.data[0]['end_year'], 2020)
-        self.assertIn('id', response.data[0])
+        first_exp = response.data['results'][0] if 'results' in response.data else response.data[0]
+        self.assertEqual(first_exp['company_name'], 'Company A')
+        self.assertEqual(first_exp['position'], 'Developer')
+        self.assertEqual(first_exp['start_year'], 2018)
+        self.assertEqual(first_exp['end_year'], 2020)
+        self.assertIn('id', first_exp)
 
     def test_get_single_job_experience(self):
-        url = f"{self.url}{self.experience1.id}/"
-        response = self.client.get(url)
+        response = self.client.get(self.url)
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data['company_name'], 'Company A')
-
-    def test_unauthorized_access(self):
-        self.client.force_authenticate(user=self.user)
-        response = self.client.get(self.url)
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['id'], self.experience1.id)
 
     def test_job_experience_ordering(self):
         response = self.client.get(self.url)
-        self.assertLess(
-            response.data[0]['start_year'],
-            response.data[1]['start_year']
+        data = response.data['results'] if 'results' in response.data else response.data
+
+        self.assertLessEqual(
+            data[0]['start_year'],
+            data[1]['start_year']
         )
+
+    def test_unauthorized_access(self):
+        self.client.credentials()
+        response = self.client.get(self.url)
+        self.assertIn(response.status_code, [401, 403])
