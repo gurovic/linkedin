5646b74ddace7aafc0e2000bb434085b48b47703
andBush <bushin-08@mail.ru>
2025-04-26 13:24:38 +0300

    university_student added

--- web/app/serializers/angular_uneditable_account_serializer.py
+++ web/app/serializers/angular_uneditable_account_serializer.py
@@ -1,24 +1,32 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 from .university_serializer import UniversitySerializer
+from .universitystudent_serializer import UniversityStudentSerializer
 from .school_serializer import SchoolSerializer
 from ..models import UniversityStudent, StudentSchool
 
 class AngularUserDetailSerializer(serializers.ModelSerializer):
     school = serializers.SerializerMethodField()
     university = serializers.SerializerMethodField()
+    university_student = serializers.SerializerMethodField()
     first_name = User.first_name
     last_name = User.last_name
 
     class Meta:
         model = User
-        fields = ['first_name', 'last_name', 'university', 'school']
+        fields = ['first_name', 'last_name', 'university', 'university_student', 'school']
 
     def get_university(self, obj):
         university_student = UniversityStudent.objects.filter(student=obj)
         if not university_student:
             return None
         return UniversitySerializer([us.university for us in university_student], many=True).data
+
+    def get_university_student(self, obj):
+        university_student = UniversityStudent.objects.filter(student=obj)
+        if not university_student:
+            return None
+        return UniversityStudentSerializer(university_student, many=True).data
     
     def get_school(self, obj):
         student_school = StudentSchool.objects.filter(student=obj)
